33639a015ff4776ef11622615edc027ea3836bd0
anty <anty150@gmail.com>
2017-01-30 22:33:35 +0100

    calibrated auto driving;

--- auto_drive.py
+++ auto_drive.py
@@ -3,6 +3,7 @@
 from ev3dev.auto import *
 
 print('Setting input values')
+MIN_MOTOR_SPEED = 20
 LEFT_MOTOR_PORT = OUTPUT_B
 RIGHT_MOTOR_PORT = OUTPUT_C
 SCANNER_MOTOR_PORT = OUTPUT_A
@@ -38,53 +39,105 @@ def run():
     scan_results = [MAX_DISTANCE, MAX_DISTANCE, MAX_DISTANCE]
     scan_integrals = [0, 0, 0]
 
-    scan_result_tmp = MAX_DISTANCE
-    scan_result_tmp_pos = 1
+    scan_result_tmp = [MAX_DISTANCE, 1]
 
     def rotateScanner(target):
-        SCANNER_MOTOR.run_to_abs_pos(position_sp=target * SCANNER_MOTOR_GEAR_RATIO)
+        SCANNER_MOTOR.run_to_abs_pos(speed_sp=SCANNER_MOTOR.max_speed / 5,
+                                     position_sp=target * SCANNER_MOTOR_GEAR_RATIO)
 
     def getActualScannerPos():
         scanner_position = SCANNER_MOTOR.position
-        if scanner_position < -25:
+        if scanner_position < -30:
             return 0
-        if scanner_position > 25:
+        if scanner_position > 30:
             return 2
         return 1
 
     def writeTmpResult(result, scanner_pos):
-        global scan_result_tmp, scan_result_tmp_pos
-        if scanner_pos != scan_result_tmp_pos:
-            scan_results[scan_result_tmp_pos] = scan_result_tmp
-            scan_integrals[scan_result_tmp_pos] = scan_result_tmp + scan_integrals[scan_result_tmp_pos] / 2
-            scan_result_tmp = MAX_DISTANCE
-            scan_result_tmp_pos = scanner_pos
+        if scanner_pos != scan_result_tmp[1]:
+            scan_results[scan_result_tmp[1]] = scan_result_tmp[0]
+            scan_integrals[scan_result_tmp[1]] = scan_result_tmp[0] + scan_integrals[scan_result_tmp[1]] / 2
+            scan_result_tmp[0] = MAX_DISTANCE
+            scan_result_tmp[1] = scanner_pos
             updateDrive()
 
-        scan_result_tmp = min(scan_result_tmp, result)
+        scan_result_tmp[0] = min(scan_result_tmp[0], result)
 
-        if scan_result_tmp < scan_results[scan_result_tmp_pos]:
-            scan_results[scan_result_tmp_pos] = scan_result_tmp
+        if scan_result_tmp[0] < scan_results[scan_result_tmp[1]]:
+            scan_results[scan_result_tmp[1]] = scan_result_tmp[0]
             updateDrive()
 
     def updateDrive():
-        speed_mul = (scan_results[1] * 3 - MAX_DISTANCE) / MAX_DISTANCE * 100
-        left_speed = ((scan_results[2] / scan_results[0]) + (scan_integrals[2] / scan_integrals[0])) / 2 * speed_mul
-        right_speed = ((scan_results[0] / scan_results[2]) + (scan_integrals[0] / scan_integrals[2])) / 2 * speed_mul
+        results_left_ratio = scan_results[2] / (scan_results[0] if scan_results[0] != 0 else 0.1)
+        results_right_ratio = scan_results[0] / (scan_results[2] if scan_results[2] != 0 else 0.1)
 
-        if scan_results[2] < scan_results[0]:
-            right_speed *= 1.2
+        speed_mul = (scan_results[1] * 4 - MAX_DISTANCE * 0.7) / MAX_DISTANCE
+        # if abs(speed_mul) < 0.25:
+        #     if speed_mul == 0:
+        #         speed_mul = -0.25
+        #     else:
+        #         speed_mul *= 0.25 / abs(speed_mul)
+        left_speed = results_left_ratio * speed_mul * 100
+        right_speed = results_right_ratio * speed_mul * 100
 
-        if scan_results[0] < scan_results[2]:
-            left_speed *= 1.2
+        slow_speed_mul = (MAX_DISTANCE * 0.7 + scan_results[1] * 0.3) / MAX_DISTANCE
+        if scan_results[2] > scan_results[0]:
+            right_speed *= slow_speed_mul
+        else:
+            left_speed *= slow_speed_mul
 
-        LEFT_MOTOR.duty_cycle_sp = min(max(left_speed, -100), 100)
-        RIGHT_MOTOR.duty_cycle_sp = min(max(right_speed, -100), 100)
+        if speed_mul < 0:
+            left_speed, right_speed = right_speed, left_speed
+
+        left_speed = min(max(left_speed, -100), 100) * 0.4
+        right_speed = min(max(right_speed, -100), 100) * 0.4
+
+        if abs(left_speed) < MIN_MOTOR_SPEED and abs(right_speed) < MIN_MOTOR_SPEED:
+            if abs(left_speed) < abs(right_speed):
+                mul = MIN_MOTOR_SPEED / right_speed
+                left_speed *= mul
+                right_speed *= mul
+            else:
+                if right_speed == 0:
+                    if left_speed == 0:
+                        left_speed = -MIN_MOTOR_SPEED
+                        right_speed = -MIN_MOTOR_SPEED
+                    else:
+                        left_speed *= MIN_MOTOR_SPEED / left_speed
+                else:
+                    mul = MIN_MOTOR_SPEED / left_speed
+                    left_speed *= mul
+                    right_speed *= mul
+
+        LEFT_MOTOR.duty_cycle_sp = left_speed
+        RIGHT_MOTOR.duty_cycle_sp = right_speed
+
+    # def updateDrive():
+    #     results_left_ratio = (scan_results[2] / scan_results[0]) if scan_results[0] != 0 else 1
+    #     results_right_ratio = (scan_results[0] / scan_results[2]) if scan_results[2] != 0 else 1
+    #     integrals_left_ratio = scan_integrals[2] / scan_integrals[0] if scan_integrals[0] != 0 else results_left_ratio
+    #     integrals_right_ratio = scan_integrals[0] / scan_integrals[2] if scan_integrals[2] != 0 else results_right_ratio
+    #
+    #     speed_mul = (scan_results[1] + MAX_DISTANCE / 2) / MAX_DISTANCE
+    #     left_speed = (2 * results_left_ratio + integrals_left_ratio) / 3 * speed_mul * 100
+    #     right_speed = (2 * results_right_ratio + integrals_right_ratio) / 3 * speed_mul * 100
+    #
+    #     slow_speed_mul = (scan_results[1] / MAX_DISTANCE) * 3 - 1
+    #     if scan_results[2] > scan_results[0]:
+    #         right_speed *= slow_speed_mul
+    #     else:
+    #         left_speed *= slow_speed_mul
+    #
+    #     LEFT_MOTOR.duty_cycle_sp = min(max(left_speed, -100), 100) / 4
+    #     RIGHT_MOTOR.duty_cycle_sp = min(max(right_speed, -100), 100) / 4
+    #     print(str(scan_results), str(scan_integrals), left_speed, right_speed, LEFT_MOTOR.duty_cycle_sp, RIGHT_MOTOR.duty_cycle_sp)
 
     try:
-        rotateScanner(80)
-        SCANNER_MOTOR.wait_until('running')
-        rotateScanner(-80)
+        rotateScanner(90)
+        while 'running' in SCANNER_MOTOR.state:
+            pass
+
+        rotateScanner(-90)
         while 'running' in SCANNER_MOTOR.state:
             writeTmpResult(DISTANCE_SENSOR.value(), getActualScannerPos())
         next_positive = True
@@ -92,13 +145,14 @@ def run():
         LEFT_MOTOR.run_direct()
         RIGHT_MOTOR.run_direct()
         while True:
-            scanner_target = 80 if next_positive else -80
+            scanner_target = 90 if next_positive else -90
             next_positive = not next_positive
             rotateScanner(scanner_target)
             while 'running' in SCANNER_MOTOR.state:
                 writeTmpResult(DISTANCE_SENSOR.value(), getActualScannerPos())
-    except Exception:
+    except KeyboardInterrupt:
         pass
+
     LEFT_MOTOR.stop()
     RIGHT_MOTOR.stop()
 
@@ -110,7 +164,9 @@ print('Exiting...')
 LEFT_MOTOR.reset()
 RIGHT_MOTOR.reset()
 
-SCANNER_MOTOR.stop_action = 'hold'
-SCANNER_MOTOR.run_to_abs_pos(position_sp=0)
-SCANNER_MOTOR.wait_until('running')
-SCANNER_MOTOR.reset()
+SCANNER_MOTOR.run_to_abs_pos(speed_sp=SCANNER_MOTOR.max_speed / 10, position_sp=0)
+while 'running' in SCANNER_MOTOR.state:
+    pass
+SCANNER_MOTOR.stop_action = 'coast'
+SCANNER_MOTOR.stop()
+# SCANNER_MOTOR.reset()
